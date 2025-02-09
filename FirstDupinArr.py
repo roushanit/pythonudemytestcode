9fba02c8760fcad6e1b739ce365c4d717cdaf47f
roushanit <roushansingh1992@gmail.com>
2025-02-09 16:15:35 +0530

    Find and show any duplicate in array

---
+++ FirstDupinArr.py
@@ -0,0 +1,17 @@
+import array
+
+def find_duplicate(nums):
+    num_set = set()
+
+    for n in nums:
+        if n in num_set:
+            return n
+        else:
+            num_set.add(n)
+
+    else:
+        return -1
+
+arr = array.array('i',[2,3,4,5,6,7,15,8,9,10,5])
+print(find_duplicate(arr))            
+    
