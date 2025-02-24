b6e541afdfb5ff6b03506f6dd91024d308eb37b0
roushanit <roushansingh1992@gmail.com>
2025-02-24 22:07:49 +0530

    Adding GFG code in repos

---
+++ GFGcode/swap1andlast.py
@@ -0,0 +1,16 @@
+# Swap function
+def swapList(list):
+    
+    # Storing the first and last element 
+    # as a pair in a tuple variable get
+    get = list[-1], list[0]
+    
+    # unpacking those elements
+    list[0], list[-1] = get
+    
+    return list
+    
+# Driver code
+newList = [12, 35, 9, 56, 24]
+print(swapList(newList))
+
